13cbdc5e50a10a4b621fdba9d8c1486eab5aa520
Thomas Ringling <github@ringling.info>
2026-03-06 15:00:09 +0100

    release.ex with seeds

--- lib/release.ex
+++ lib/release.ex
@@ -3,11 +3,36 @@ defmodule Jikan.Release do
 
   def migrate do
     load_app()
+
     for repo <- repos() do
       {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
     end
   end
 
-  defp repos, do: Application.fetch_env!(@app, :ecto_repos)
-  defp load_app, do: Application.load(@app)
+  def seed(file \\ "seeds.exs") do
+    load_app()
+
+    seed_file = Path.join([:code.priv_dir(@app), "repo", file])
+
+    if File.exists?(seed_file) do
+      Code.eval_file(seed_file)
+      IO.puts("Seeds from #{file} executed successfully")
+    else
+      IO.puts("Seed file not found: #{seed_file}")
+      {:error, :not_found}
+    end
+  end
+
+  def rollback(repo, version) do
+    load_app()
+    {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
+  end
+
+  defp repos do
+    Application.fetch_env!(@app, :ecto_repos)
+  end
+
+  defp load_app do
+    Application.load(@app)
+  end
 end
