4f9b74590a6f77a251cb05e3f99b152cf3f57cdb
Thomas Ringling <github@ringling.info>
2026-03-07 17:54:19 +0100

    inet removed

--- config/runtime.exs
+++ config/runtime.exs
@@ -31,8 +31,8 @@ if config_env() == :prod do
   config :jikan, Jikan.Repo,
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    hostname: System.get_env("DATABASE_HOST") || "localhost",
-    socket_options: [:inet6]
+    hostname: System.get_env("DATABASE_HOST") || "localhost"
+    #socket_options: [:inet6]
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
